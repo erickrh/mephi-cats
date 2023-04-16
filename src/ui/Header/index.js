3c78d16a28863e2bfd3ea760ce8f539cdb1acd7e
Erick <123erickrh@gmail.com>
2023-04-16 10:19:04 -0500

    Change header color and add a black cat icon

--- src/ui/Header/index.js
+++ src/ui/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { ReactComponent as BlackCat } from '../icons/cat-black-face.svg';
 
 function Header() {
   return (
     <>
       <div className='headerContainer'>
         <h1 className='header'>Mephi Cats</h1>
+        <BlackCat className='BlackCat' />
       </div>
     </>
   );
