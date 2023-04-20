df56edb681f131ee499a5b0f9f6e9af6a6719272
Erick <123erickrh@gmail.com>
2023-04-20 10:03:22 -0500

    Add a button in header to go home

--- src/ui/Header/index.js
+++ src/ui/Header/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Header.css';
 import { ReactComponent as BlackCat } from '../icons/cat-black-face.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+  const goHome = () => navigate('/');
+
   return (
     <>
       <div className='headerContainer'>
-        <h1 className='header'>Mephi Cats</h1>
-        <BlackCat className='BlackCat' />
+        <h1 onClick={goHome} className='header'>Mephi Cats</h1>
+        <BlackCat onClick={goHome} className='BlackCat' />
       </div>
     </>
   );
